cfca89af07c9d955738cb87ff8d9df198c36e314
Armaghan Haque <armaghan.haque.25@gmail.com>
2026-08-12 18:08:16 +0530

    Add typedarray polyfill and util-deprecate module
    - Introduced `typedarray` package with polyfill for TypedArrays, including package.json, readme, and tests.
    - Added `util-deprecate` module for deprecation warnings with browser support, including necessary files and documentation.
    - Implemented storage service for file uploads using ImageKit.

--- proj-2/src/controllers/music.controller.js
+++ proj-2/src/controllers/music.controller.js
@@ -1,4 +1,6 @@
 const musicModel = require('../models/music.model')
+const {uploadFile} = require('../service/stoarge.service')
+const jwt = require('jsonwebtoken')
 
 async function createMusic(req, res) {
 
@@ -16,13 +18,38 @@ async function createMusic(req, res) {
 
         if(decoded.role !== 'artist') {
             return res.status(403).json({
-                message : "Forbidden"
+                message : "You don't have permission to create music"
             })
         }
 
-    } catch(error) {
+
+
+    const {title} = req.body
+    const file = req.file
+
+    const result = await uploadFile(file.buffer.toString('base64'))
+
+    const music = await musicModel.create({
+        uri: result.url,
+        title,
+        artist: decoded.id
+    })
+
+    res.status(201).json({
+        message : "Music created successfully",
+        music :{
+            id: music._id,
+            uri: music.uri,
+            title: music.title,
+            artist: music.artist
+        }
+    })  
+
+        } catch(error) {
         return res.status(401).json({
             message : "Unauthorized"
         })
     }
 }
+
+module.exports = { createMusic }
