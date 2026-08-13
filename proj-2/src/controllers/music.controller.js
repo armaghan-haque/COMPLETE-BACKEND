275a38d939db8a34df06700fd51b8d1600d9efc5
Armaghan Haque <armaghan.haque.25@gmail.com>
2026-08-13 17:08:24 +0530

    Add album model and update music routes

--- proj-2/src/controllers/music.controller.js
+++ proj-2/src/controllers/music.controller.js
@@ -1,5 +1,6 @@
 const musicModel = require('../models/music.model')
 const {uploadFile} = require('../service/stoarge.service')
+const albumModel = require('../models/album.model')
 const jwt = require('jsonwebtoken')
 
 async function createMusic(req, res) {
@@ -52,4 +53,49 @@ async function createMusic(req, res) {
     }
 }
 
-module.exports = { createMusic }
+async function createAlbum(req, res) {
+
+    const token = req.cookies.token
+
+    if(!token) {
+        return res.status(401).json({
+            message : "Unauthorized"
+        })
+    }
+
+    try{
+
+        const decoded = jwt.verify(token, process.env.JWT_SECRET)
+
+        if(decoded.role !== 'artist') {
+            return res.status(403).json({
+                message : "You don't have permission to create album"
+            })
+        }
+
+        const {title , musics} = req.body
+
+        const album = await albumModel.create({ 
+            title,
+            musics: musics,
+            artist: decoded.id
+        })
+
+        res.status(201).json({
+            message : "Album created successfully",
+            album :{
+                id: album._id,
+                title: album.title,
+                musics: album.musics,
+                artist: album.artist
+            }
+        })
+
+    } catch(error) {
+        return res.status(401).json({
+            message : "Unauthorized"
+        })
+    }
+}
+
+module.exports = { createMusic , createAlbum }
--- proj-2/src/routes/music.routes.js
+++ proj-2/src/routes/music.routes.js
@@ -12,4 +12,6 @@ const router = express.Router()
 
 router.post('/upload', upload.single('music'), musicController.createMusic)
 
+router.post('/album' , musicController.createAlbum)
+
  module.exports = router
